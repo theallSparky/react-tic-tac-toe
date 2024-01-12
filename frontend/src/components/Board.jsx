02a70e6271c65374eb7db69bac320ddc1c70d756
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-01-11 18:45:46 -0600

    Made board look awesome!

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -5,13 +5,13 @@ function Board() {
   return (
     <div className="board">
       <Tile className="right-border bottom-border" />
-      <Tile />
-      <Tile />
-      <Tile />
-      <Tile />
-      <Tile />
-      <Tile />
-      <Tile />
+      <Tile className="right-border bottom-border" />
+      <Tile className="bottom-border" />
+      <Tile className="right-border bottom-border" />
+      <Tile className="right-border bottom-border" />
+      <Tile className="bottom-border" />
+      <Tile className="right-border" />
+      <Tile className="right-border" />
       <Tile />
       <Strike />
     </div>
