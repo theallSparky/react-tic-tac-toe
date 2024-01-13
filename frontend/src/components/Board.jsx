f29174075df20967edc60a228d556bfff0d80e37
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-01-12 23:34:52 -0600

    Added multiple onClicks to board

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -1,18 +1,50 @@
 import Strike from "./Strike";
 import Tile from "./Tile";
 
-function Board({ tiles }) {
+function Board({ tiles, onTileClick }) {
   return (
     <div className="board">
-      <Tile value={tiles[0]} className="right-border bottom-border" />
-      <Tile value={tiles[1]} className="right-border bottom-border" />
-      <Tile value={tiles[2]} className="bottom-border" />
-      <Tile value={tiles[3]} className="right-border bottom-border" />
-      <Tile value={tiles[4]} className="right-border bottom-border" />
-      <Tile value={tiles[5]} className="bottom-border" />
-      <Tile value={tiles[6]} className="right-border" />
-      <Tile value={tiles[7]} className="right-border" />
-      <Tile value={tiles[8]} />
+      <Tile
+        onClick={() => onTileClick(0)}
+        value={tiles[0]}
+        className="right-border bottom-border"
+      />
+      <Tile
+        onClick={() => onTileClick(1)}
+        value={tiles[1]}
+        className="right-border bottom-border"
+      />
+      <Tile
+        onClick={() => onTileClick(2)}
+        value={tiles[2]}
+        className="bottom-border"
+      />
+      <Tile
+        onClick={() => onTileClick(3)}
+        value={tiles[3]}
+        className="right-border bottom-border"
+      />
+      <Tile
+        onClick={() => onTileClick(4)}
+        value={tiles[4]}
+        className="right-border bottom-border"
+      />
+      <Tile
+        onClick={() => onTileClick(5)}
+        value={tiles[5]}
+        className="bottom-border"
+      />
+      <Tile
+        onClick={() => onTileClick(6)}
+        value={tiles[6]}
+        className="right-border"
+      />
+      <Tile
+        onClick={() => onTileClick(7)}
+        value={tiles[7]}
+        className="right-border"
+      />
+      <Tile onClick={() => onTileClick(8)} value={tiles[8]} />
       <Strike />
     </div>
   );
--- frontend/src/components/TicTacToe.jsx
+++ frontend/src/components/TicTacToe.jsx
@@ -6,11 +6,15 @@ const PLAYER_O = "O";
 
 export const TicTacToe = () => {
   const [tiles, setTiles] = useState(Array(9).fill(null));
-  const [playerTurn, setPlayerTurn] = useState();
+  const [playerTurn, setPlayerTurn] = useState(PLAYER_X);
+
+  const handleTileClick = (index) => {
+    console.log(index);
+  };
   return (
     <div>
       <h1>Tic Tac Toe</h1>
-      <Board tiles={tiles} />
+      <Board tiles={tiles} onTileClick={handleTileClick} />
     </div>
   );
 };
