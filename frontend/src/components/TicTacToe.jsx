6799267e1883e172ac0c991a5765a31cf99d50d6
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-01-13 01:17:09 -0600

    Added playerTurn={playerTurn} to each tile in board.jsx

--- frontend/src/components/TicTacToe.jsx
+++ frontend/src/components/TicTacToe.jsx
@@ -25,7 +25,11 @@ export const TicTacToe = () => {
   return (
     <div>
       <h1>Tic Tac Toe</h1>
-      <Board tiles={tiles} onTileClick={handleTileClick} />
+      <Board
+        playerTurn={playerTurn}
+        tiles={tiles}
+        onTileClick={handleTileClick}
+      />
     </div>
   );
 };
