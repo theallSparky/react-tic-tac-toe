7fc47d40346ff502cb7ccb1034a48f4be951cc65
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-01-14 18:04:19 -0600

    Fixed bug where more tiles can be clicked after win condition is triggered

--- frontend/src/components/TicTacToe.jsx
+++ frontend/src/components/TicTacToe.jsx
@@ -54,6 +54,9 @@ export const TicTacToe = () => {
   const [gameState, setGameState] = useState(GameState.inProgress);
 
   const handleTileClick = (index) => {
+    if (gameState !== GameState.inProgress) {
+      return;
+    }
     if (tiles[index] !== null) {
       return;
     }
